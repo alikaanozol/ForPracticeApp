574d9cd530c9d81d502e64a149b39f5363f4b092
KAAN <alikaanozoll@gmail.com>
2024-07-21 12:05:14 +0300

    Add project files.

---
+++ ForPracticeApp/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("-----< Alıştırma 1 >-----");
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine($"{i}) Kendime inanıyorum, ben bu yazılım işini hallederim!");
+}
+
+Console.WriteLine("-----< Alıştırma 2 >-----");
+for (int i = 1;i < 21; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("-----< Alıştırma 3 >-----");
+for (int i = 2; i < 21; i+=2)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("-----< Alıştırma 4 >-----");
+int total = 0;
+for (int i = 50;i < 151; i++)
+{
+    total=total+i;
+}
+Console.WriteLine("50 ile 150 arasındaki sayıların toplamı" + " " + total);
+
+Console.WriteLine("-----< Alıştırma 5 >-----");
+//tek sayıların toplamı(1-120)
+int single= 0;
+for (int i = 1; i <120; i+=2)
+{
+    single=single+i;
+}
+Console.WriteLine("1 ile 120 arasındaki tek sayıların toplamı" + " " + single);
+//çift sayıların toplamı(1-120)
+int couple = 0;
+for (int i = 2; i < 121; i+=2)
+{
+    couple = couple + i;
+}
+Console.WriteLine("1 ile 120 arasındaki çift sayıların toplamı" + " " + couple);
